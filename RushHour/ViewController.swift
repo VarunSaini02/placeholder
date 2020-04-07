28bde26a9900f5ec596af9c88152815ecd4a370e
Underdoneboar4 <blake@branvold.com>
2020-04-07 13:34:45 -0500

    Initial Commit

---
+++ RushHour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RushHour
+//
+//  Created by 90304593 on 4/7/20.
+//  Copyright © 2020 90304593. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
