2470a50c7b5326f9138236c2a19ab6021e4ea007
Underdoneboar4 <blake@branvold.com>
2020-04-08 13:04:54 -0500

    outlets added for tiles

--- RushHour/ViewController.swift
+++ RushHour/ViewController.swift
@@ -18,6 +18,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x1y1: UIImageView!
+    @IBOutlet weak var x2y1: UIImageView!
+    @IBOutlet weak var x3y1: UIImageView!
+    @IBOutlet weak var x4y1: UIImageView!
+    @IBOutlet weak var x5y1: UIImageView!
+    @IBOutlet weak var x6y1: UIImageView!
+    
+    @IBOutlet weak var x1y2: UIImageView!
+    @IBOutlet weak var x2y2: UIImageView!
+    @IBOutlet weak var x3y2: UIImageView!
+    @IBOutlet weak var x4y2: UIImageView!
+    @IBOutlet weak var x5y2: UIImageView!
+    @IBOutlet weak var x6y2: UIImageView!
+    
+    @IBOutlet weak var x1y3: UIImageView!
+    @IBOutlet weak var x2y3: UIImageView!
+    @IBOutlet weak var x3y3: UIImageView!
+    @IBOutlet weak var x4y3: UIImageView!
+    @IBOutlet weak var x5y3: UIImageView!
+    @IBOutlet weak var x6y3: UIImageView!
+    
+    @IBOutlet weak var x1y4: UIImageView!
+    @IBOutlet weak var x2y4: UIImageView!
+    @IBOutlet weak var x3y4: UIImageView!
+    @IBOutlet weak var x4y4: UIImageView!
+    @IBOutlet weak var x5y4: UIImageView!
+    @IBOutlet weak var x6y4: UIImageView!
+    
+    @IBOutlet weak var x1y5: UIImageView!
+    @IBOutlet weak var x2y5: UIImageView!
+    @IBOutlet weak var x3y5: UIImageView!
+    @IBOutlet weak var x4y5: UIImageView!
+    @IBOutlet weak var x5y5: UIImageView!
+    @IBOutlet weak var x6y5: UIImageView!
+    
+    @IBOutlet weak var x1y6: UIImageView!
+    @IBOutlet weak var x2y6: UIImageView!
+    @IBOutlet weak var x3y6: UIImageView!
+    @IBOutlet weak var x4y6: UIImageView!
+    @IBOutlet weak var x5y6: UIImageView!
+    @IBOutlet weak var x6y6: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
