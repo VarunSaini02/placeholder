5c57985e342771574b5b4f73f4a5c569564d3edf
VarunSaini02 <varun.saini@gmail.com>
2020-04-11 20:59:35 -0500

    Moved Car/Coordinate to own Swift file and made Level's swift file.

---
+++ RushHour/Level.swift
@@ -0,0 +1,13 @@
+//
+//  Level.swift
+//  RushHour
+//
+//  Created by Blake Branvold and Varun Saini on 4/11/20.
+//  Copyright © 2020 Blake Branvold and Varun Saini. All rights reserved.
+//
+
+import UIKit
+
+class Level {
+    
+}
